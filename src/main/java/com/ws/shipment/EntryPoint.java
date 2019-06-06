67e6a66e80086505457ccc63c701beb62c95b4f5
Tushar Panda <tusharkpandaccpp@gmail.com>
2019-06-06 10:26:45 -0700

    Added comments and tests

--- src/main/java/com/ws/shipment/EntryPoint.java
+++ src/main/java/com/ws/shipment/EntryPoint.java
@@ -5,8 +5,18 @@ import java.util.List;
 import com.ws.shipment.models.ZipCodeRange;
 import com.ws.shipment.service.ShipmentService;
 
+/**
+ * @author tushpa
+ *
+ */
 public class EntryPoint {
 
+	
+	/**
+	 * @param args
+	 * 
+	 * @description Entrypoint of the application. Takes input from command line and feeds it to ShipmentService.
+	 */
 	public static void main(String[] args) {
 		ShipmentService shipmentService = new ShipmentService();
 		List<ZipCodeRange> range = shipmentService.processRanges(args);
--- src/main/java/com/ws/shipment/service/ShipmentService.java
+++ src/main/java/com/ws/shipment/service/ShipmentService.java
@@ -1,7 +1,11 @@
 package com.ws.shipment.service;
 
+import static com.ws.shipment.constants.ServiceConstants.BLANKS;
+import static com.ws.shipment.constants.ServiceConstants.COMMA;
 import static com.ws.shipment.constants.ServiceConstants.INCORRECT_RANGE_ERROR;
-import static com.ws.shipment.constants.ServiceConstants.*;
+import static com.ws.shipment.constants.ServiceConstants.RANGE_VALIDATION_ERROR;
+import static com.ws.shipment.constants.ServiceConstants.NON_WORD_CHAR_REGEX;
+import static com.ws.shipment.constants.ServiceConstants.VALID_RANGE_REGEX;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -11,34 +15,56 @@ import java.util.List;
 import com.ws.shipment.models.ZipCodeRange;
 
 public class ShipmentService {
-	
-    public List<ZipCodeRange> processRanges(String[] args) {
-		List<ZipCodeRange> range = new ArrayList<>();
-		Arrays.stream(args).forEach(i -> {
-			i = i.replaceAll(SQUARE_BRACKET_REGEX, BLANKS);
-			validateInput(i.trim());
-			String[] rangeArr = i.split(COMMA);
-			range.add(validateRangeOrder(new ZipCodeRange(rangeArr[0], rangeArr[1])));
-		});
 
-		//Find overlapping zip ranges
-		fetchRestrictedZipRanges(range);
+
+	/**
+	 * @param args - Array containing the zip code ranges coming in from command line
+	 * @return - List of the zip code ranges representing overlaps in the input ranges, if any.
+	 */
+	public List<ZipCodeRange> processRanges(String[] args) {
+		List<ZipCodeRange> range = new ArrayList<>();
+		if (args != null) {
+			Arrays.stream(args).forEach(i -> {
+				// replaces all [ or ] and spaces from the input.
+				i = i.replaceAll(NON_WORD_CHAR_REGEX, BLANKS);
+				validateInput(i);
+				
+				// splits the input range by comma
+				String[] rangeArr = i.split(COMMA);
+				ZipCodeRange zipCodeRange = new ZipCodeRange(rangeArr[0], rangeArr[1]);
+				validateRangeOrder(zipCodeRange);
+				range.add(zipCodeRange);
+			});
+
+			// Find overlapping zip ranges
+			computeZipRanges(range);
+		}
 		return range;
 	}
-	
-	private void fetchRestrictedZipRanges(List<ZipCodeRange> range) {
-		range.sort((ZipCodeRange o1, ZipCodeRange  o2) -> o1.getStartingZip().compareTo(o2.getStartingZip()));
-		for(int i = 0; i < range.size()-1 ; i ++) {
-			if(isOverlapping(range.get(i),range.get(i+1))) {
-				mergeRanges(range.get(i),range.get(i+1));
-				range.remove(i+1);
+
+	/**
+	 * @param range - List of all valid zip code ranges which needs to be sorted and merged,
+	 * in case the consecutive list elements are overlapping
+	 */
+	private void computeZipRanges(List<ZipCodeRange> range) {
+		// sorts the zip codes
+		range.sort((ZipCodeRange o1, ZipCodeRange o2) -> o1.getStartingZip().compareTo(o2.getStartingZip()));
+		
+		// find overlaps and merge
+		for (int i = 0; i < range.size() - 1; i++) {
+			if (isOverlapping(range.get(i), range.get(i + 1))) {
+				mergeRanges(range.get(i), range.get(i + 1));
+				range.remove(i + 1);
 				i--;
 			}
 		}
-		
+
 	}
-	
-	
+
+	/**
+	 * @param first - ZipCodeRange
+	 * @param later - ZipCodeRange
+	 */
 	private void mergeRanges(ZipCodeRange first, ZipCodeRange later) {
 		if (first.getStartingZip() <= later.getStartingZip()) {
 			first.setStartingZip(first.getStartingZip());
@@ -52,21 +78,33 @@ public class ShipmentService {
 			first.setEndingZip(later.getEndingZip());
 		}
 	}
-	
-	
+
+	/**
+	 * @param first -ZipCodeRange
+	 * @param later - ZipCodeRange
+	 * @return returns true if the two zip code ranges have an overlap, else returns false.
+	 */
 	private boolean isOverlapping(ZipCodeRange first, ZipCodeRange later) {
 		return (later.getStartingZip() >= first.getStartingZip() && later.getStartingZip() <= first.getEndingZip())
 				|| (later.getEndingZip() >= first.getStartingZip() && later.getEndingZip() <= first.getEndingZip());
 	}
 
-	private ZipCodeRange validateRangeOrder(ZipCodeRange zipCodeRange) {
+	/**
+	 * @param zipCodeRange
+	 * @description Checks if the ZipCodeRange has starting zipCode greater than ending zipCode.
+	 * If true, throws a RuntimeException
+	 */
+	private void validateRangeOrder(ZipCodeRange zipCodeRange) {
 		if (zipCodeRange.getStartingZip() > zipCodeRange.getEndingZip()) {
 			throw new RuntimeException(MessageFormat.format(INCORRECT_RANGE_ERROR,
 					zipCodeRange.getStartingZip().toString(), zipCodeRange.getEndingZip().toString()));
 		}
-		return zipCodeRange;
 	}
 
+	/**
+	 * @param range
+	 * @description Checks if the zipcode range provided are 5 digit each and are separated by a comma.
+	 */
 	private void validateInput(String range) {
 		if (!range.matches(VALID_RANGE_REGEX)) {
 			throw new RuntimeException(MessageFormat.format(RANGE_VALIDATION_ERROR, range));
